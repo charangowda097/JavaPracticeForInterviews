d769ea6be6f32f59a4a16311eda4afe719f021de
charangowda097 <118718467+charangowda097@users.noreply.github.com>
2024-02-03 12:34:39 +0530

    This is the Second Commit from My Local to see the Changes

--- src/com/charan/pracExample/Main/DemoGit.java
+++ src/com/charan/pracExample/Main/DemoGit.java
@@ -3,5 +3,6 @@ package com.charan.pracExample.Main;
 public class DemoGit {
     public static void main(String[] args) {
         System.out.println("Hello World Git");
+        System.out.println("This is the Second Commit");
     }
 }
